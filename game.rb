fbfa120016bff21853ac2cae81a18f626d60fca0
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-04-06 19:59:38 -0500

    Check for vertical/horizontal winner and exit loop

--- game.rb
+++ game.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module HandleBoard
+  private
+
   SYMBOLS = { 0 => '+', 1 => 'x', 2 => 'o' }.freeze
   def draw_board(board)
     puts ''
@@ -28,8 +30,8 @@ module HandleBoard
 
   def numpad_to_index(numpad)
     # messy translation from numpad to board
-    row = nil
-    column = nil
+    # row = nil
+    # column = nil
     case numpad
     when 1 then row = 2
                 column = 0
@@ -54,12 +56,58 @@ module HandleBoard
   end
 
   def find_winner(_board)
+    # initialize array to store columns for winner check
+    columns = [[], [], []]
+    col_idx = 0
+    # return value if all values in array match
+    def match_value(array)
+      return array[0] if array.uniq.length == 1
+
+      # if not matching return -1
+      -1
+    end
+    _board.each_with_index do |row, _row_idx|
+      # return the value if row is all same and not zero
+      matching_value = match_value(row)
+      return matching_value if matching_value.positive?
+
+      # construct arrays of columns for matching later
+      row.each_with_index do |place, col_idx|
+        columns[col_idx].push(place)
+      end
+    end
+    columns.each do |column|
+      # return the value if column is all same and not zero
+      matching_value = match_value(column)
+      return matching_value if matching_value.positive?
+    end
     0
   end
 end
 
 class Game
   include HandleBoard
+  # start game
+  def start
+    # initialize player variable
+    player = 2 # swaps to 1 in play_turn
+    # begin first turn before loop
+    play_turn
+    loop do
+      # check for winner
+      winner = find_winner(board)
+      if winner.positive?
+        print_board
+        print_winner(winner)
+        break
+      end
+      # play turn in a loop until winner check succeeds
+      play_turn
+    end
+  end
+
+  private
+
   def initialize
     @board = [
       [0, 0, 0],
@@ -74,34 +122,29 @@ class Game
     draw_board(board)
   end
 
-  def start
-    player = 2
-    print_board
-    next_turn
+  def print_winner(winner)
+    winner_char = winner == 1 ? 'x' : 'o'
+    puts "#{winner_char} wins!"
   end
-
-  def game_winner
-    find_winner(board)
-  end
-
-  private
 
   def get_choice
     loop do
       player_choice = gets.chomp
       # check if input is number
-      is_number = !player_choice.match(/\A[0-9]\Z/).nil?
-      p is_number
+      is_valid_number = !player_choice.match(/\A[0-9]\Z/).nil?
       # check if input already placed
       is_not_taken = true
-      if is_number
+      # check if spot has already been taken
+      if is_valid_number
         player_choice
         row, column = numpad_to_index(player_choice.to_i)
         is_not_taken = !is_taken?(board[row][column])
       end
-      return player_choice.to_i if is_number && is_not_taken
+      # return player integer if valid input
+      return player_choice.to_i if is_valid_number && is_not_taken
 
-      unless is_number
+      # display numpad for reference if not a valid number
+      unless is_valid_number
         puts 'Use the numpad format to select a placement:'
         puts '789'
         puts '456'
@@ -112,20 +155,20 @@ class Game
   end
 
   # call next turn recursively until winner
-  def next_turn
+  def play_turn
     # swap player
     @player = (@player == 1 ? 2 : 1)
     # get player choice
     letter = @player == 1 ? 'x' : 'o'
+    print_board
     puts "#{letter}'s turn. Enter a numpad key to place an #{letter}."
     # get player choice
     player_choice = get_choice
     # place player choice on board
     place_choice(board, player, player_choice)
-    print_board
-    next_turn unless game_winner.positive?
   end
 end
 
+# create game instance and start
 game = Game.new
 game.start
